ad48fa51af7fce78611c07a7556b5bbf5cd9fd7b
Anyona228 <echpochmak78@gmail.com>
2025-01-19 16:10:38 +1000

    upload projects page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -7,23 +7,23 @@ const Projects: React.FC = () => {
             <h2>Мои проекты</h2>
             <div className="projects-grid">
                 <div className="project-card">
-                    <a href="https://github.com" target="_blank" rel="noopener noreferrer">
-                        ч 1
+                    <a href="https://github.com/AnyonaDavydova/PersonalSite" target="_blank" rel="noopener noreferrer">
+                        Персональный сайт (новый)
                     </a>
                 </div>
                 <div className="project-card">
-                    <a href="https://github.com" target="_blank" rel="noopener noreferrer">
-                        ч 2
+                    <a href="https://github.com/AnyonaDavydova/DAVYDOVA-ANYONA---site" target="_blank" rel="noopener noreferrer">
+                        Персональный сайт (старый)
                     </a>
                 </div>
                 <div className="project-card">
-                    <a href="https://github.com" target="_blank" rel="noopener noreferrer">
-                        ч 3
+                    <a href="https://github.com/AnyonaDavydova/WoodLand" target="_blank" rel="noopener noreferrer">
+                        Браузерная игра
                     </a>
                 </div>
                 <div className="project-card">
-                    <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
-                        ч 4
+                    <a href="https://yandex.ru/images/search?from=tabbar&img_url=https%3A%2F%2Fpbs.twimg.com%2Fmedia%2FFLUTzJkXsAYexBo.jpg&lr=75&pos=7&rpt=simage&text=котенок%20мем" target="_blank" rel="noopener noreferrer">
+                        Продолжение следует...
                     </a>
                 </div>
             </div>
