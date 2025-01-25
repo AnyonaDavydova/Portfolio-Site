ddcbfd3d99255f596964ead4c75af9ce86d127a8
Anyona228 <echpochmak78@gmail.com>
2025-01-25 18:22:56 +1000

    update styles and fix bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Skills from './pages/Skills';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import { Routes, Route } from "react-router-dom"
+import {Header} from "./components/Header"
+import {Home} from "./pages/Home"
+import {About} from "./pages/About"
+import {Contact} from "./pages/Contact"
+import {Projects} from "./pages/Projects"
+import {Skills} from "./pages/Skills"
+import { Footer } from "./components/Footer"
+import { socialLinks } from "./data/SocialLinks"
+export function App() {
 
 
-const App: React.FC = () => {
     return (
-        <>
-            <Header />
+        <div>
+            <Header/>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/skills" element={<Skills />} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/contact" element={<Contact />} />
+                <Route path="/" element={<Home/>} />
+                <Route path="/about" element={<About/>} />
+                <Route path="/skills" element={<Skills/>} />
+                <Route path="/projects" element={<Projects/>} />
+                <Route path="/contact" element={<Contact/>} />
             </Routes>
-            <Footer />
-        </>
-    );
-};
+            <Footer socialLinks={socialLinks} email ="echpochmak78@gmail.com"/>
+        </div>
 
-export default App;
+    )
+}
