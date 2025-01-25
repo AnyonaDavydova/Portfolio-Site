38c7d6f9cd5bf328ff60c91b261a0f2e52eec83f
Anyona228 <echpochmak78@gmail.com>
2025-01-25 17:38:33 +1000

    add types and data

---
+++ src/data/Projects.ts
@@ -0,0 +1,24 @@
+import { IProject } from '../types/Project';
+ export const projects: IProject[] = [
+    {
+        id: 1,
+        title: 'Персональный сайт',
+        description: 'Сайт для демонстрации умений ANONы, version 1',
+        technologies: ['Vue', 'TypeScript'],
+        link: 'https://github.com/AnyonaDavydova/DAVYDOVA-ANYONA---site',
+    },
+    {
+        id: 2,
+        title: 'Персональный сайт',
+        description: 'Сайт для демонстрации умений ANONы, version 2',
+        technologies: ['Vue', 'TypeScript', 'React'],
+        link: 'https://github.com/AnyonaDavydova/PersonalSite',
+    },
+    {
+        id: 3,
+        title: 'Woodland',
+        description: 'Браузерная игра, вдохновленная Geometry Dash',
+        technologies: ['Vue','TypeScript', 'Electron'],
+        link: 'https://github.com/AnyonaDavydova/WoodLand',
+    }
+ ];
---
+++ src/data/SocialLinks.ts
@@ -0,0 +1,24 @@
+import { ISocialLink} from "../types/SocialLink"
+import githubIcon from "../assets/Icons/Cgit.png"
+import telegramIcon from "../assets/Icons/Ctg.png"
+import vkIcon from "../assets/Icons/Cvk.png"
+
+
+export const socialLinks: ISocialLink[]=[
+    {
+        iconSrc: githubIcon,
+        url: 'https://github.com/AnyonaDavydova',
+        label: 'GitHub'
+      },
+      {
+        iconSrc:  telegramIcon,
+        url: 'https://t.me/aaaanyonaaaa',
+        label: 'Telegram'
+      },
+      {
+        iconSrc: vkIcon,
+        url: 'https://vk.com/lifeisnotsoyahoooo',
+        label: 'VK'
+      }
+]
+
